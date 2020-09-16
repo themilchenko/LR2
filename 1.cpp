1942710360ed61e1d99c1345297bec7661e745bf
Ivan <70635146+themilchenko@users.noreply.github.com>
2020-09-16 22:15:20 +0300

    Exercise 1

---
+++ 1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int i;
+    for (i = 0; i < 256; i++) {
+        char n = i;
+        cout « i « " " « n « endl;
+    }
+    return 0;
+}
